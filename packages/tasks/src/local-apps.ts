603e0fd6b9eb3d7afc647b145f335438ab637bcd
Mishig Davaadorj <dmishig@gmail.com>
2024-10-07 11:39:33 +0200

    [Local App Snippet] support non conversational LLMs

--- packages/tasks/src/local-apps.ts
+++ packages/tasks/src/local-apps.ts
@@ -83,15 +83,19 @@ function isLlamaCppGgufModel(model: ModelData) {
 }
 
 const snippetLlamacpp = (model: ModelData, filepath?: string): LocalAppSnippet[] => {
-	const command = (binary: string) =>
-		[
+	const command = (binary: string) => {
+		let snippet = [
 			"# Load and run the model:",
 			`${binary} \\`,
 			`  --hf-repo "${model.id}" \\`,
 			`  --hf-file ${filepath ?? "{{GGUF_FILE}}"} \\`,
-			'  -p "You are a helpful assistant" \\',
-			"  --conversation",
+			`  -p "${model.tags.includes("conversational") ? "You are a helpful assistant" : "Once upon a time "}"`,
 		].join("\n");
+		if (model.tags.includes("conversational")) {
+			snippet += " \\\n  --conversation";
+		}
+		return snippet;
+	};
 	return [
 		{
 			title: "Install from brew",
@@ -143,7 +147,7 @@ const snippetLocalAI = (model: ModelData, filepath?: string): LocalAppSnippet[] 
 };
 
 const snippetVllm = (model: ModelData): LocalAppSnippet[] => {
-	const runCommand = [
+	const runCommandInstruct = [
 		"# Call the server using curl:",
 		`curl -X POST "http://localhost:8000/v1/chat/completions" \\`,
 		`	-H "Content-Type: application/json" \\`,
@@ -154,6 +158,18 @@ const snippetVllm = (model: ModelData): LocalAppSnippet[] => {
 		`		]`,
 		`	}'`,
 	];
+	const runCommandNonInstruct = [
+		"# Call the server using curl:",
+		`curl -X POST "http://localhost:8000/v1/completions" \\`,
+		`	-H "Content-Type: application/json" \\`,
+		`	--data '{`,
+		`		"model": "${model.id}",`,
+		`		"prompt": "Once upon a time ",`,
+		`		"max_tokens": 512,`,
+		`		"temperature": 0.5`,
+		`	}'`,
+	];
+	const runCommand = model.tags.includes("conversational") ? runCommandInstruct : runCommandNonInstruct;
 	return [
 		{
 			title: "Install from pip",
